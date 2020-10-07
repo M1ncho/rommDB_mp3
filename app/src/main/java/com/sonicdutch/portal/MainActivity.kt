eacd849b6948610d75981d1f21081e8d116899f9
Jiwan Kim <neve0630@blifeinc.com>
2020-10-07 17:56:42 +0900

    화면 배치 완료, 클릭시 해당 uri로 이동, as팝업창 띄우기 완료

--- app/src/main/java/com/sonicdutch/portal/MainActivity.kt
+++ app/src/main/java/com/sonicdutch/portal/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.sonicdutch.portal
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +16,81 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        tv_home.setOnClickListener {
+            var in_home = Intent(Intent.ACTION_VIEW, Uri.parse("https://sonicdutch.modoo.at/"))
+            startActivity(in_home)
+        }
 
+        tv_way.setOnClickListener {
+            var in_useway = Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/IptQbHSC4I0"))
+            startActivity(in_useway)
+        }
+
+
+        tv_Maintenance.setOnClickListener {
+            var in_as = Intent(Intent.ACTION_VIEW, Uri.parse("http://palmcloud.co.kr/sonicdutch/view/contact_register.html"))
+            startActivity(in_as)
+        }
+
+        tv_q_an.setOnClickListener {
+            var in_qanswer = Intent(Intent.ACTION_VIEW,Uri.parse("https://sonicdutch.modoo.at/?link=aa5s09di"))
+            startActivity(in_qanswer)
+        }
+
+
+        tv_recipes.setOnClickListener {
+            var in_recipes = Intent(Intent.ACTION_VIEW, Uri.parse("https://sonicdutch.modoo.at/?link=bv2huhtg"))
+            startActivity(in_recipes)
+        }
+
+
+        iv_face.setOnClickListener {
+            var in_face = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/soniccoldbrew"))
+            startActivity(in_face)
+        }
+
+        iv_instar.setOnClickListener {
+            var in_instar = Intent(Intent.ACTION_VIEW,Uri.parse("https://www.instagram.com/sonicdutch"))
+            startActivity(in_instar)
+        }
+
+
+        iv_kakao.setOnClickListener {
+            var in_kakao = Intent(Intent.ACTION_VIEW, Uri.parse("https://pf.kakao.com/_tiMxlxl"))
+            startActivity(in_kakao)
+        }
+
+        iv_blog.setOnClickListener {
+            var in_blog = Intent(Intent.ACTION_VIEW, Uri.parse("https://blog.naver.com/sonicdutch"))
+            startActivity(in_blog)
+        }
+
+
+        
+        tv_call.setOnClickListener {
+
+            var builder = AlertDialog.Builder(this)
+
+            var poplayout = layoutInflater.inflate(R.layout.popup_menu,null)
+
+            var call_tv : TextView=poplayout.findViewById(R.id.tv_as_call)
+            var send_tv : TextView=poplayout.findViewById(R.id.tv_as_send)
+
+            call_tv.setOnClickListener {
+
+                Toast.makeText(this,"테스트",Toast.LENGTH_SHORT).show()
+            }
+
+            send_tv.setOnClickListener {
+
+                Toast.makeText(this,"테스트",Toast.LENGTH_SHORT).show()
+            }
+
+
+            builder.setView(poplayout)
+            builder.create()
+            builder.show()
+        }
 
 
 
