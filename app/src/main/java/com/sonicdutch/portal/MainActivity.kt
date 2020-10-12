2c207b586e5cfaca96f3a795924f958099701f53
Jiwan Kim <neve0630@blifeinc.com>
2020-10-12 16:48:09 +0900

    남은 url연결 완료

--- app/src/main/java/com/sonicdutch/portal/MainActivity.kt
+++ app/src/main/java/com/sonicdutch/portal/MainActivity.kt
@@ -1,25 +1,69 @@
 package com.sonicdutch.portal
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.text.DateFormat
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
+import java.text.SimpleDateFormat
 
 
 class MainActivity : AppCompatActivity() {
 
-    val now: Long = System.currentTimeMillis()
-    val date = Date(now)
+    //현 시각
+    val date_now = SimpleDateFormat("yyyy-MM-dd")
+    val time_now = SimpleDateFormat("HH")
 
+
+    //현 위치
+    var locationmanger : LocationManager? = null
+    private val REQUEST_CODE_LOCATION : Int = 2
+    var latitude : Double? = null
+    var longitude : Double? =null
+
+    private fun getCurrentLoc()
+    {
+        locationmanger = getSystemService(Context.LOCATION_SERVICE)as LocationManager?
+        var userlocation: Location = getLatLang()
+        if(userlocation!= null)
+        {
+            latitude = userlocation.latitude
+            longitude = userlocation.longitude
+            Log.d("cheak","$latitude, $longitude" )
+
+        }
+        
+    }
+
+
+    private fun getLatLang(): Location {
+
+        var currentLatLng: Location? = null
+        if(ActivityCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), this.REQUEST_CODE_LOCATION)
+            getLatLang()
+        }
+        else
+        {
+            val locationProvider = LocationManager.GPS_PROVIDER
+            currentLatLng = locationmanger?.getLastKnownLocation(locationProvider)
+        }
+        return currentLatLng!!           //currentLatLng가 null이 아님을 표시
+    }
+
+
+
+    //날씨...검사
 
 
 
@@ -78,6 +122,25 @@ class MainActivity : AppCompatActivity() {
             startActivity(in_blog)
         }
 
+        tv_notice.setOnClickListener {
+
+            var in_notice = Intent(Intent.ACTION_VIEW, Uri.parse("https://sonicdutch.modoo.at/?link=3x3fk95u"))
+            startActivity(in_notice)
+        }
+
+        iv_video.setOnClickListener {
+
+            var in_video = Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/z_3FokLgjT4"))
+            startActivity(in_video)
+        }
+
+
+        tv_download.setOnClickListener {
+
+            var in_down = Intent(Intent.ACTION_VIEW, Uri.parse("http://palmcloud.co.kr/sonicdutch/module/manualDownload.php"))
+            startActivity(in_down)
+        }
+
 
 
         tv_call.setOnClickListener {
@@ -101,7 +164,7 @@ class MainActivity : AppCompatActivity() {
                 val magnum = Uri.parse("sms:010-7233-0754")
                 var magintent = Intent(Intent.ACTION_SENDTO,magnum)
                 startActivity(magintent)
-                Toast.makeText(this,"테스트",Toast.LENGTH_SHORT).show()
+
             }
 
             builder.setView(poplayout)
